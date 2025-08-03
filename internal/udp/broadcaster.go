d0f2010fd732251828096877fc015df50a82e01b
Adwaith <adwaithtry@gmail.com>
2025-08-03 19:24:42 +0530

    Phase 1 complited , now we can send udp message

--- internal/udp/broadcaster.go
+++ internal/udp/broadcaster.go
@@ -1,1 +1,56 @@
 package udp
+
+import (
+	"fmt"
+	"net"
+	"strings"
+	"time"
+)
+
+func getLocalIP() (string, error) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return "", err
+	}
+
+	for _, addr := range addrs {
+		ipNet, ok := addr.(*net.IPNet)
+		ip := ipNet.IP
+		if ok && !ip.IsLoopback() && ip.To4() != nil {
+			return ip.String(), nil
+		}
+	}
+
+	return "", fmt.Errorf("cannot find non-loopback IP address")
+}
+
+func StartBroadcast(name string, port int) {
+	addr := fmt.Sprintf("255.255.255.255:%d", port)
+	udpAddr, err := net.ResolveUDPAddr("udp", addr)
+	if err != nil {
+		fmt.Println("Error resolving UDP address:", err)
+		return
+	}
+
+	conn, err := net.DialUDP("udp", nil, udpAddr)
+	if err != nil {
+		fmt.Println("Error dialing UDP : ", err)
+		return
+	}
+	defer conn.Close()
+	localIP, err := getLocalIP()
+	if err != nil {
+		fmt.Println("Error to get localIP : ", err)
+		return
+	}
+	for {
+		message := fmt.Sprintf("%s|%s", strings.TrimSpace(name), localIP)
+		_, err := conn.Write([]byte(message))
+		if err != nil {
+			fmt.Println("Error in sending UDP broadcast", err)
+		} else {
+			fmt.Println("Broadcasted : ", message)
+		}
+		time.Sleep(2 * time.Second)
+	}
+}
--- main.go
+++ main.go
@@ -1,1 +1,7 @@
 package main
+
+import "github.com/Adwaith-NP/dropzone/internal/udp"
+
+func main() {
+	udp.StartBroadcast("adwaith", 9090)
+}
