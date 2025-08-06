8d9e5762a3c7571bf1724c6335eab56f8c2670c1
Adwaith <adwaithtry@gmail.com>
2025-08-06 10:06:31 +0530

    add the file sanitation

--- internal/utils/fileutils.go
+++ internal/utils/fileutils.go
@@ -1,1 +1,43 @@
 package utils
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+// Return true if the file path exist
+func PathExists(path string) bool {
+	_, err := os.Stat(path)
+	return !os.IsNotExist(err)
+}
+
+// Return true if the path point a directory
+func IsDirectory(path string) bool {
+	info, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+	return info.IsDir()
+}
+
+// Return the file same as string
+func GetAllFiles(dir string) ([]string, error) {
+	var files []string
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			files = append(files, path)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	if len(files) == 0 {
+		return nil, fmt.Errorf("no files found in directory")
+	}
+	return files, nil
+}
--- main.go
+++ main.go
@@ -3,13 +3,21 @@ package main
 import (
 	"fmt"
 
-	"github.com/Adwaith-NP/dropzone/internal/udp"
+	"github.com/Adwaith-NP/dropzone/internal/utils"
 )
 
 const UDP_IP = 9090
 const DEFAULT_NAME = "DropZone"
 
 func main() {
-	ip, err := udp.StartListening(UDP_IP)
-	fmt.Println(ip, err)
+	// udp.StartBroadcast(DEFAULT_NAME, UDP_IP)
+	// ip, err := udp.StartListening(UDP_IP)
+	// fmt.Println(ip, err)
+	files, err := utils.GetAllFiles("/Users/adwaith/Documents/dropzone")
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, i := range files {
+		fmt.Println(i)
+	}
 }
